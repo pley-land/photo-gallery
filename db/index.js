d2df0e587738c06dcf098408dcbfcb6a35d60af0
Steven Lee <Steven@Blueberry.local>
2018-07-20 15:10:22 -0700

    Setup database connection

---
+++ db/index.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'pley'
+});
+
+connection.connect();
+
+module.exports = connection;
