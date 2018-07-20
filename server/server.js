d5cda85aff879d339e88a06e55fe21f2b4d7adca
Steven Lee <Steven@Blueberry.local>
2018-07-20 16:34:35 -0700

    Serve static html with server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("public"));
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3005, () => console.log('Till 3005, hold up'));
