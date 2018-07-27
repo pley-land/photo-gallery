55d83228192bfa77ea7de28a04d887391f44d2ec
Steven Lee <stevenyslee@gmail.com>
2018-07-26 19:54:49 -0700

    Add index test

--- client/index.test.jsx
+++ client/index.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import PhotoGallery from './photogallery';
+import PhotoGallery from './components/photogallery';
 
 
 Enzyme.configure({ adapter: new Adapter() });
