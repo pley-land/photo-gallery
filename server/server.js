75bfa55e96c98e6243818671b074128508f8d4ea
Steven Lee <stevenyslee@gmail.com>
2018-07-27 19:20:10 -0700

    Remove unnecessary comments

--- server/server.js
+++ server/server.js
@@ -4,24 +4,12 @@ const db = require('./../db/index');
 
 const app = express();
 
-// app.use('/biz/', express.static('public'));
-// app.get('/biz/', (req, res) => res.send('Hello World!'));
 app.use('/', express.static('public'));
 
-// Expose API
 app.get('/biz/:restaurant', (req, res) => {
   console.log(req.params);
   db.getPictures(req.params.restaurant, res);
 });
-
-// app.get('/biz/:restaurant', (req, res) => {
-//   console.log(req.params);
-//   db.getPictures(req.params.restaurant, res);
-// });
-
-// app.use('/biz/:restaurant', (req, res) => {
-//   console.log('test');
-// });
 
 app.listen(3005, () => console.log('Server listening on port 3005'));
 
