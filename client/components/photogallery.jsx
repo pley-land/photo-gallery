ca81f842cb03dee20f7fb564ad755bf9c12a9b49
Steven Lee <stevenyslee@gmail.com>
2018-07-30 20:14:53 -0700

    Add tests and refactor code

--- client/components/photogallery.jsx
+++ client/components/photogallery.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
 import searchBarIMG from '../../public/StaticSearch.png';
 import mapIMG from '../../public/StaticMap.png';
 import squares from '../../public/4squares.jpg';
@@ -16,35 +17,11 @@ class PhotoGallery extends React.Component {
       photoIndex: 0,
       isOpen: false,
     };
-
-    this.enlargePicture = this.enlargePicture.bind(this);
-    this.shrinkPicture = this.shrinkPicture.bind(this);
     this.onClickHandler = this.onClickHandler.bind(this);
   }
 
   onClickHandler() {
     this.setState({ isOpen: true });
-  }
-
-  enlargePicture(e) {
-    if (e.target.nodeName === 'SPAN') {
-      $('#img2').removeClass('selected');
-      $('#img2').addClass('default');
-      $('#img3').addClass('selected');
-    } else {
-      $('#img2').removeClass('selected');
-      $('#img2').addClass('default');
-      $(e.target).addClass('selected');
-    }
-  }
-
-  shrinkPicture() {
-    $('#img2').addClass('selected');
-    $('#img2').removeClass('default');
-    $('#img3').removeClass('selected');
-    $('#img3').addClass('default');
-    $('#img1').removeClass('selected');
-    $('#img1').addClass('default');
   }
 
   render() {
@@ -79,10 +56,28 @@ class PhotoGallery extends React.Component {
                 id="img1"
                 className="default photo"
                 onClick={this.onClickHandler}
-                onMouseEnter={this.enlargePicture}
-                onMouseLeave={this.shrinkPicture}
+                onMouseEnter={(e) => {
+                  if (e.target.nodeName === 'SPAN') {
+                    $('#img2').removeClass('selected');
+                    $('#img2').addClass('default');
+                    $('#img3').addClass('selected');
+                  } else {
+                    $('#img2').removeClass('selected');
+                    $('#img2').addClass('default');
+                    $(e.target).addClass('selected');
+                  }
+                }}
+                onMouseLeave={() => {
+                  $('#img2').addClass('selected');
+                  $('#img2').removeClass('default');
+                  $('#img3').removeClass('selected');
+                  $('#img3').addClass('default');
+                  $('#img1').removeClass('selected');
+                  $('#img1').addClass('default');
+                }}
+                onKeyDown={this.handleKeyDown}
                 src=""
-                alt="Could be portrait or landscape"
+                alt=""
               />
               {isOpen && (
                 <Lightbox
@@ -107,18 +102,54 @@ class PhotoGallery extends React.Component {
                 id="img2"
                 className="selected photo"
                 onClick={this.onClickHandler}
-                onMouseEnter={this.enlargePicture}
-                onMouseLeave={this.shrinkPicture}
+                onMouseEnter={(e) => {
+                  if (e.target.nodeName === 'SPAN') {
+                    $('#img2').removeClass('selected');
+                    $('#img2').addClass('default');
+                    $('#img3').addClass('selected');
+                  } else {
+                    $('#img2').removeClass('selected');
+                    $('#img2').addClass('default');
+                    $(e.target).addClass('selected');
+                  }
+                }}
+                onMouseLeave={() => {
+                  $('#img2').addClass('selected');
+                  $('#img2').removeClass('default');
+                  $('#img3').removeClass('selected');
+                  $('#img3').addClass('default');
+                  $('#img1').removeClass('selected');
+                  $('#img1').addClass('default');
+                }}
+                onKeyDown={this.handleKeyDown}
                 src=""
-                alt="Could be portrait or landscape"
+                alt=""
               />
             </div>
             <div id="photo3">
               <span
                 className="centered shadow"
                 onClick={this.onClickHandler}
-                onMouseEnter={this.enlargePicture}
-                onMouseLeave={this.shrinkPicture}
+                onMouseEnter={(e) => {
+                  if (e.target.nodeName === 'SPAN') {
+                    $('#img2').removeClass('selected');
+                    $('#img2').addClass('default');
+                    $('#img3').addClass('selected');
+                  } else {
+                    $('#img2').removeClass('selected');
+                    $('#img2').addClass('default');
+                    $(e.target).addClass('selected');
+                  }
+                }}
+                onMouseLeave={() => {
+                  $('#img2').addClass('selected');
+                  $('#img2').removeClass('default');
+                  $('#img3').removeClass('selected');
+                  $('#img3').addClass('default');
+                  $('#img1').removeClass('selected');
+                  $('#img1').addClass('default');
+                }}
+                onKeyDown={this.handleKeyDown}
               >
                 <img
                   id="squares"
@@ -132,10 +163,28 @@ class PhotoGallery extends React.Component {
                 id="img3"
                 className="default photo"
                 onClick={this.onClickHandler}
-                onMouseEnter={this.enlargePicture}
-                onMouseLeave={this.shrinkPicture}
+                onMouseEnter={(e) => {
+                  if (e.target.nodeName === 'SPAN') {
+                    $('#img2').removeClass('selected');
+                    $('#img2').addClass('default');
+                    $('#img3').addClass('selected');
+                  } else {
+                    $('#img2').removeClass('selected');
+                    $('#img2').addClass('default');
+                    $(e.target).addClass('selected');
+                  }
+                }}
+                onMouseLeave={() => {
+                  $('#img2').addClass('selected');
+                  $('#img2').removeClass('default');
+                  $('#img3').removeClass('selected');
+                  $('#img3').addClass('default');
+                  $('#img1').removeClass('selected');
+                  $('#img1').addClass('default');
+                }}
+                onKeyDown={this.handleKeyDown}
                 src=""
-                alt="Could be portrait or landscape"
+                alt=""
               />
             </div>
           </div>
